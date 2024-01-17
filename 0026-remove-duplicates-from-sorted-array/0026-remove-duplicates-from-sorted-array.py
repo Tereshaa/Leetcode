5f20261ed9e2aeb91d4f082478a2b838973d3d98
Y Teresha <106382420+Tereshaa@users.noreply.github.com>
2024-01-18 00:00:03 +0530

    Time: 72 ms (91.42%), Space: 18.7 MB (17.83%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -7,7 +7,7 @@ class Solution:
             if nums[i]!=nums[j]:
                 i=i+1
                 nums[i]=nums[j]
-        i=i+1
-        return i
+        
+        return i+1
                 
         
