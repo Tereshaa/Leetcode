523543898253dc55f64af3ff1e56975df6e2fd12
Y Teresha <106382420+Tereshaa@users.noreply.github.com>
2024-01-18 21:07:39 +0530

    Time: 495 ms (97.26%), Space: 31.8 MB (42.53%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -11,5 +11,27 @@ class Solution:
             if(sum<0):
                 sum=0
         return maxi
-            
+    
+    
+#     to find subarray
+#         n=len(nums)
+#         start=0
+#         ansstart=-1
+#         ansend=-1
+    
+#         maxi = -sys.maxsize - 1
+#         sum=0
+#         for i in range(n):
+#             if sum==0:
+#                 start=i
+#             sum=sum+nums[i]
+#             if(sum>maxi):
+#                 maxi=sum
+#                 ansstart=start
+#                 ansend=i
+#             if(sum<0):
+#                 sum=0
+#         for i in range(ansstart,ansend+1):
+#             print(nums[i])
+         
         
