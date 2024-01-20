eac950ebb1fba4b15c03343b371bc85f4e2b3f08
Y Teresha <106382420+Tereshaa@users.noreply.github.com>
2024-01-20 13:15:32 +0530

    Time: 1031 ms (77.19%), Space: 47.7 MB (49.56%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,16 +1,32 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        negative=[]
-        positive=[]
         n=len(nums)
-        final=[]*n
+        ans=[0]*n
+        pos=0
+        neg=1
         for i in range(n):
             if nums[i]<0:
-                negative.append(nums[i])
+                ans[neg]=nums[i]
+                neg=neg+2
             elif nums[i]>0:
-                positive.append(nums[i])
-        for i in range(len(positive)):
-            nums[2*i]=positive[i]
-        for i in range(len(negative)):
-            nums[2*i+1]=negative[i]
-        return nums
+                ans[pos]=nums[i]
+                pos=pos+2
+        return ans
+        
+        
+        
+#         brute force
+        # negative=[]
+        # positive=[]
+        # n=len(nums)
+        # final=[]*n
+        # for i in range(n):
+        #     if nums[i]<0:
+        #         negative.append(nums[i])
+        #     elif nums[i]>0:
+        #         positive.append(nums[i])
+        # for i in range(len(positive)):
+        #     nums[2*i]=positive[i]
+        # for i in range(len(negative)):
+        #     nums[2*i+1]=negative[i]
+        # return nums
