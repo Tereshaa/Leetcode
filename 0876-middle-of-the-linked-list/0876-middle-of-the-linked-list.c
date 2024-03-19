832d1e34643bf00dfc0223ddcb50ddaf522eae5a
Y Teresha <106382420+Tereshaa@users.noreply.github.com>
2024-03-19 22:00:12 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (27.19%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.c
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.c
@@ -6,17 +6,27 @@
  * };
  */
 struct ListNode* middleNode(struct ListNode* head) {
-    int count=0;
-    struct ListNode* temp= head;
-    while(temp!=NULL){
-        count=count+1;
-        temp=temp->next;
+    //tortoise hare method
+    struct ListNode* slow=head;
+    struct ListNode* fast=head;
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
     }
-    count=count/2;
-    temp=head;
-    for (int i=0;i<count;i++){
-        temp=temp->next;
-    }
-    return temp;
+    return slow;
+    
+    
+    // int count=0;
+    // struct ListNode* temp= head;
+    // while(temp!=NULL){
+    //     count=count+1;
+    //     temp=temp->next;
+    // }
+    // count=count/2;
+    // temp=head;
+    // for (int i=0;i<count;i++){
+    //     temp=temp->next;
+    // }
+    // return temp;
     
 }
