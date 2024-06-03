9717ce16d9c5d8f512e2576ebdd840826f3c08e1
Y Teresha <106382420+Tereshaa@users.noreply.github.com>
2024-06-03 18:04:37 +0530

    Time: 61 ms (56.86%), Space: 17.8 MB (38.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,33 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n=len(nums)
-        numindices=[(nums[i],i)for i in range(n)]
-        numindices.sort()
-        left=0
-        right=n-1  
-        while(left<right):
-            sum=numindices[left][0]+numindices[right][0]
-            if sum<target:
-                left=left+1
-            elif sum>target:
-                right=right-1
-            elif sum==target:
-                return [numindices[left][1],numindices[right][1]]
+        numbers={}
+        for i,num in enumerate(nums):
+            diff=target-num
+            if diff in numbers:
+                return [numbers[diff],i]
+            numbers[num]=i
             
-            
-            
-            #brute force
-#         for i in range(n-1):
-#             for j in range(1,n):
-#                 if nums[i]+nums[j]==target and i!=j:
-#                     return [i,j]
 
-#hashmap
- # numindices={}
- #        for i,num in enumerate(nums):
- #            diff=target-num
- #            if diff in numindices:
- #                return [numindices[diff],i]
- #            numindices[num]=i
-
-        
