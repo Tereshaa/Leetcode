8b3a21e2ba10dc723d1b4de138cf227cacf84a5e
Y Teresha <106382420+Tereshaa@users.noreply.github.com>
2024-07-28 17:11:34 +0530

    Time: 9 ms (38.54%), Space: 12.4 MB (10.48%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.c
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isBalanced(struct TreeNode* root) {
+    return height(root)!=-1;
+
+}
+
+ int height(struct TreeNode* root){
+        if (root==NULL){
+            return 0;
+        }
+        int lh= height(root->left);
+        if(lh==-1) return -1;
+        int rh=height(root->right);
+        if(rh==-1) return -1;
+        
+        if (abs(lh-rh)>1) return -1;
+        return 1+(lh>rh?lh:rh);
+    }
