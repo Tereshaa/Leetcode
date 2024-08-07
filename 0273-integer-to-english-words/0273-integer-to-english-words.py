8b3bb997819c739d952d121909e10c3fc91b9645
Y Teresha <106382420+Tereshaa@users.noreply.github.com>
2024-08-07 14:39:46 +0530

    Time: 36 ms (61.63%), Space: 16.7 MB (47.47%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.py
+++ 0273-integer-to-english-words/0273-integer-to-english-words.py
@@ -3,7 +3,6 @@ class Solution:
         if num == 0:
             return "Zero"
         
-        # Mapping numbers to words
         less_than_20 = {
             1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 
             8: "Eight", 9: "Nine", 10: "Ten", 11: "Eleven", 12: "Twelve", 13: "Thirteen", 
