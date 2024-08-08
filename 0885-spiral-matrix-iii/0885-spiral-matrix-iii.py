da63227fd612d8073eff97841617b05a900f7ade
Y Teresha <106382420+Tereshaa@users.noreply.github.com>
2024-08-09 00:00:23 +0530

    Time: 83 ms (57.42%), Space: 17.8 MB (74.61%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
@@ -5,23 +5,16 @@ class Solution:
         res = []
         i = 0
         r, c = rStart, cStart
-        
-        # Continue until we have covered all cells in the grid
         while len(res) < rows * cols:
-            for _ in range(2):  # Each cycle (step) consists of two passes: e.g., right and down
+            for _ in range(2): 
                 dr, dc = directions[i]
                 for _ in range(steps):
-                    # Only add the coordinate if it's within the bounds
                     if 0 <= r < rows and 0 <= c < cols:
                         res.append([r, c])
-                    # Move to the next cell in the current direction
                     r += dr
                     c += dc
-                # Change direction after finishing a segment of the spiral
                 i = (i + 1) % 4
-            # Increase step size after completing a full right-down-left-up cycle
             steps += 1
-        
         return res
 
     
