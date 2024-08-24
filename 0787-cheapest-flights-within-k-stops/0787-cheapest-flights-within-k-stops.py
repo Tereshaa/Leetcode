7858bd0ae0a4d323e3d51af2230150e766cb7e79
Y Teresha <106382420+Tereshaa@users.noreply.github.com>
2024-08-24 12:12:39 +0530

    Time: 100 ms (57.95%), Space: 17.9 MB (60.93%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -3,34 +3,22 @@ class Solution:
         graph = {i:[] for i in range(n)}
         for u, v, w in flights:
             graph[u].append((v, w))
-        
-        # Initialize DP array with infinity
-        dp = [float('inf')] * n
-        dp[src] = 0
-        
-        # Initialize a queue for BFS
-        queue = deque([(src, 0)])  # (current city, current cost)
-        
-        # Perform BFS up to k+1 levels
-        level = 0
-        while queue and level <= k:
-            size = len(queue)
-            current_dp = dp[:]  # Copy current DP array to update costs at this level
             
-            for _ in range(size):
-                u, current_cost = queue.popleft()
-                
-                # Explore the neighbors of the current city
-                for v, w in graph[u]:
-                    if current_cost + w < current_dp[v]:
-                        current_dp[v] = current_cost + w
-                        queue.append((v, current_dp[v]))
-            
-            dp = current_dp  # Update DP array after this level
-            level += 1
+        dp=[float("inf")]*n
+        dp[src]=0
+        queue=deque([(src,0)]) #current city,current cost
         
-        # The minimum cost to reach `dst` after at most k stops
-        return dp[dst] if dp[dst] != float('inf') else -1
-
+        levels=0
+        while queue and levels<=k:
+            current_dp=dp[:]
+            for _ in range(len(queue)):
+                node,curr_cost=queue.popleft()
+                for v,w in graph[node]:
+                    if curr_cost+w<current_dp[v]:
+                        current_dp[v]=curr_cost+w
+                        queue.append((v,current_dp[v]))
+            dp=current_dp
+            levels+=1
+        return dp[dst] if dp[dst]!=float("inf") else -1
         
-        
+      
