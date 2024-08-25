050c8e2e6e4763bf5fa0e5fc36e76bc23e4a5e27
Y Teresha <106382420+Tereshaa@users.noreply.github.com>
2024-08-25 18:25:02 +0530

    Time: 30 ms (83.62%), Space: 16.4 MB (59.17%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -2,10 +2,13 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n<=1:
             return 1
-        last=1
-        seclast=1
-        for i in range(2,n+1):
-            current=last+seclast
-            last=seclast
-            seclast=current
+        prev1=1
+        prev2=1
+        for _ in range(2,n+1):
+            current=prev1+prev2
+            prev2=prev1
+            prev1=current
         return current
+        
+        
+      
