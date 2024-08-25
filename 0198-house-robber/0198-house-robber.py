d585d885f600b05e334b945ca1ad161e90c114a1
Y Teresha <106382420+Tereshaa@users.noreply.github.com>
2024-08-26 00:21:28 +0530

    Time: 27 ms (95.61%), Space: 16.4 MB (67.90%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,18 +1,29 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n=len(nums)
-        dp=[-1 for _ in range(n)]
-        def rob(i):
-            if i==0:
-                return nums[i]
-            if i<0:
-                return 0
-            if dp[i]!=-1:
-                return dp[i]
-            pick=nums[i]+rob(i-2)
-            nopick=0+rob(i-1)
-            dp[i]= max(pick,nopick)
-            return dp[i]
-        return rob(n-1)
-          
-        
+        prev2=0
+        prev1=nums[0]
+        for i in range(1,n):
+            
+            if i>0:
+                pick=nums[i] +prev2
+            nopick=0+prev1
+            cur=max(pick,nopick)
+            prev2=prev1
+            prev1=cur
+        return prev1
+
+            
+#         def rob(i):
+#             if i==0:
+#                 return nums[i]
+#             if i<0:
+#                 return 0
+#             if dp[i]!=-1:
+#                 return dp[i]
+#             pick=nums[i]+rob(i-2)
+#             nopick=0+rob(i-1)
+#             dp[i]= max(pick,nopick)
+#             return dp[i]
+#         return rob(n-1)
+    
