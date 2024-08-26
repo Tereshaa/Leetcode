3ae7dbbac08251a5f25b8ba19e0d48ee1b8503c1
Y Teresha <106382420+Tereshaa@users.noreply.github.com>
2024-08-26 11:48:45 +0530

    Time: 42 ms (15.60%), Space: 16.5 MB (27.02%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,14 +1,33 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n=len(nums)
-        dp=[-1 for _ in range(n)]
-        dp[0]=nums[0]
-        
+        prev1=nums[0]
+        prev2=0
         for i in range(1,n):
             take=nums[i]
             if i>1:
-                take+=dp[i-2]
-            notake=0+dp[i-1]
-            dp[i]=max(take,notake)
-        return dp[n-1]
+                take+=prev2
+            notake=0+prev1
+            curr=max(take,notake)
+            prev2=prev1
+            prev1=curr
+        return prev1
+        
+        
+        
+        
+        
+        
+        #tabulization
+#         n=len(nums)
+#         dp=[-1 for _ in range(n)]
+#         dp[0]=nums[0]
+        
+#         for i in range(1,n):
+#             take=nums[i]
+#             if i>1:
+#                 take+=dp[i-2]
+#             notake=0+dp[i-1]
+#             dp[i]=max(take,notake)
+#         return dp[n-1]
             
